78ab9b516a24326196edfd5c776eed10b7821cae
Dominic Holifield <dholifi@purdue.edu>
2025-05-06 14:26:40 -0400

    add stuff to main

--- firmware/include/PCA9505.h
+++ firmware/include/PCA9505.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
-#include <Wire.h>
 #include <stdint.h>
+#include <Wire.h>
 
 #define INPUT_REG 0x00
 #define OUTPUT_REG 0x08
@@ -10,43 +10,44 @@
 #define INTERRUPT_REG 0x20
 
 class PCA9505 {
-private:
-  uint8_t _addr;
-  TwoWire &_wire;
-  static const uint8_t ports = 3;
+  private:
+    uint8_t _addr;
+    TwoWire& _wire;
+    static const uint8_t ports = 3;
 
-public:
-  PCA9505(uint8_t a0, uint8_t a1, uint8_t a2, TwoWire &wire = Wire)
-      : _wire(wire), _addr(0x20 | (a0 << 0) | (a1 << 1) | (a2 << 2)) {}
+  public:
+    uint64_t last_read = 0x00;
 
-  void begin(uint8_t sda_pin, uint8_t scl_pin) {
-    _wire.begin(sda_pin, scl_pin);
-    send_data(CONFIG_REG, 0x0F);    // Set all pins to input
-    send_data(POLARITY_REG, 0x00);  // Set all pins to normal polarity
-    send_data(INTERRUPT_REG, 0x00); // Enable interrupts
-  }
+    PCA9505(uint8_t a, TwoWire& wire = Wire) : _addr(0x20 | a), _wire(wire) {}
 
-  void send_data(uint8_t reg, uint8_t data) {
-    _wire.beginTransmission(_addr);
-    _wire.write(reg);
-    for (uint8_t i = 0; i < ports; i++) {
-      _wire.write(data);
-    }
-    _wire.endTransmission();
-  }
-
-  uint64_t read() {
-    uint64_t mask = 0;
-
-    _wire.beginTransmission(_addr);
-    _wire.write((uint8_t)INPUT_REG);
-    _wire.endTransmission(false);
-    _wire.requestFrom(_addr, ports);
-
-    for (uint8_t i = 0; i < ports; i++) {
-      mask |= ((uint64_t)_wire.read() << (i * 8));
+    void begin(uint8_t sda_pin, uint8_t scl_pin) {
+        _wire.begin(sda_pin, scl_pin);
+        send_data(CONFIG_REG, 0x0F);    // Set all pins to input
+        send_data(POLARITY_REG, 0x00);  // Set all pins to normal polarity
+        send_data(INTERRUPT_REG, 0x00); // Enable interrupts
     }
 
-    return mask;
-  }
+    void send_data(uint8_t reg, uint8_t data) {
+        _wire.beginTransmission(_addr);
+        _wire.write(reg);
+        for (uint8_t i = 0; i < ports; i++) {
+            _wire.write(data);
+        }
+        _wire.endTransmission();
+    }
+
+    uint64_t read() {
+        uint64_t mask = 0;
+
+        _wire.beginTransmission(_addr);
+        _wire.write((uint8_t)INPUT_REG);
+        _wire.endTransmission(false);
+        _wire.requestFrom(_addr, ports);
+
+        for (uint8_t i = 0; i < ports; i++) {
+            mask |= ((uint64_t)_wire.read() << (i * 8));
+        }
+
+        return mask;
+    }
 };
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,23 @@
+#include "PCA9505.h"
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define INT_PIN 0
+
+PCA9505 left(0b000, Wire);
+PCA9505 right(0b001, Wire);
+
+bool interrupted = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    left.begin(SDA, SCL);
+    right.begin(SDA, SCL);
+    attachInterrupt(digitalPintToInterrupt(INT_PIN), interrupt, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void interrupt() { interrupted = true; }
